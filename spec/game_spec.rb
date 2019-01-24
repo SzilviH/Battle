8e75307403aa86b434e7855622fa3805f94b1371
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-24 16:14:55 +0000

    skinny controllers - only 1 global variable now

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,23 @@
 require 'game'
 
 describe Game do
+  let(:archie) { double(:player, receive_damage: true) }
   let(:ozzy) { double(:player, receive_damage: true) }
+  let(:game) { described_class.new(archie, ozzy) }
+
+  it 'returns player 1' do
+    expect(game.player_1).to eq(archie)
+  end
+
+  it 'returns player 2' do
+    expect(game.player_2).to eq(ozzy)
+  end
+
 
   describe '#attack' do
     it 'damages the player' do
       expect(ozzy).to receive(:receive_damage)
-      subject.attack(ozzy)
+      game.attack(ozzy)
     end
   end
 end
